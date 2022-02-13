5689ecfe0e0a69ad5b0c455f27e7a26ecedfd6b6
Agastya Thoppur <Agastya.Thoppur@iiitb.org>
2022-02-13 16:24:19 +0530

    Added crontab functionality

--- blink_detection.py
+++ blink_detection.py
@@ -3,20 +3,55 @@ from scipy.spatial import distance
 from imutils.video import FileVideoStream
 from imutils.video import VideoStream
 from imutils import face_utils
+import pymongo
 import numpy as np
 import argparse
 import imutils
 import time
 from datetime import datetime
+from datetime import date
 import dlib
 import cv2
 import math
+import sys
+import logging
+import os
+
+
+resttime=10
+
+cur_dir=os.getcwd()
+print(cur_dir)
+
+
+dirpath=os.path.dirname(os.path.realpath(__file__))
+filename=os.path.join(dirpath,'testlog.log')
+
+logger=logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+filehandler=logging.FileHandler(filename)
+filehandler.setLevel(logging.INFO)
+filehandler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+logger.addHandler(filehandler)
+
+
+def log_resttime(resttime):
+	with open('testlog.log', 'w'):
+    		pass
+	logger.info(resttime)
+
+
+
+now = datetime.now()
+start = now.strftime("%H:%M:%S")
 
 class Blink:
     def __init__(self, startTime, duration):
         self.startTime = startTime
         self.duration = duration
-    
+
+
 STARTING_TIME = datetime.now()
 TOTAL_RUNNING_TIME = 15
 CURRENT_RUNNING_TIME = TOTAL_RUNNING_TIME
@@ -26,6 +61,7 @@ EYE_AR_THRESH = 0.28
 EYE_AR_CONSEC_FRAMES = 2
 COUNTER = 0
 ASPECT_RATIO_VECTOR = []
+
 
 def getPauseDuration(pause):
     global CURRENT_RUNNING_TIME
@@ -66,26 +102,30 @@ def eyeAspectRatio(eye):
 
 
 # initialize dlib's face detector (HOG-based) and then create the facial landmark predictor
+
+#path=str(sys.argv[1])
+#print(path)
 print("INFO: loading facial landmark predictor...")
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+predictor = dlib.shape_predictor("Desktop/Retina/shape_predictor_68_face_landmarks.dat")
 
 # get indexes of the facial landmarks for the left and right eye
 (leftEyeStart, leftEyeEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (rightEyeStart, rightEyeEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
 # start the video stream thread
+log_resttime(resttime)
 print("INFO: starting video stream thread...")
 vs = VideoStream(0).start()
 fileStream = False
 time.sleep(1.0)
 
 while True:
-    if (getDuration( STARTING_TIME, datetime.now()) >= CURRENT_RUNNING_TIME):
+    if (getDuration(STARTING_TIME, datetime.now()) >= CURRENT_RUNNING_TIME):
         break
     # Read the frame, resize it and convert to grayscale
     frame = vs.read()
-    frame = imutils.resize(frame, width = 450)
+    frame = imutils.resize(frame, width=450)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # detect faces in the grayscale frame
@@ -96,11 +136,9 @@ while True:
             STATE = "PAUSED"
             print("WARNING: face count is not equal to 1. pausing frames... ")
             PAUSE_START_TIME = datetime.now()
-        
-
     else:
         if (STATE == "PAUSED" and getPauseDuration(PAUSE_START_TIME)):
-            break
+           break
         STATE = "RUNNING"
         face = faces[0]
         shape = predictor(gray, face)
@@ -118,13 +156,13 @@ while True:
         ear = (leftEAR + rightEAR) / 2.0
 
         ASPECT_RATIO_VECTOR.append(ear)
-        
+
         if (len(ASPECT_RATIO_VECTOR) >= 60):
             ASPECT_RATIO_VECTOR.pop(0)
 
-        EYE_AR_THRESH = sum(ASPECT_RATIO_VECTOR)/len(ASPECT_RATIO_VECTOR) - 0.06
-        
-        
+        EYE_AR_THRESH = sum(ASPECT_RATIO_VECTOR) / \
+            len(ASPECT_RATIO_VECTOR) - 0.06
+
         if ear < EYE_AR_THRESH:
             COUNTER += 1
         else:
@@ -132,13 +170,13 @@ while True:
                 TOTAL_BLINKS += 1
             COUNTER = 0
 
-
-
-        cv2.putText(frame, "Blinks: {}".format(TOTAL_BLINKS), (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.putText(frame, "Blinks: {}".format(TOTAL_BLINKS),
+                    (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.putText(frame, "EAR: {:.2f}".format(
+            ear), (300, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
         # show the frame
-    cv2.imshow("Frame", frame)
+    #cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
         break
@@ -148,5 +186,13 @@ MIN_RATE = 2
 MAX_RATE = 35
 DRYNESS = 1 - (BLINK_RATE - MIN_RATE)/(MAX_RATE - MIN_RATE)
 
+Now = datetime.now()
+end = Now.strftime("%H:%M:%S")
+
+client = pymongo.MongoClient()
+mydb = client["BlinksDatabase"]
+mycol = mydb["Blinkstats"]
+mydict = {"Time ": start + " - " + end, "Date": str(date.today()), "BlinkRate": BLINK_RATE, "DrynessLevel": DRYNESS}
+mycol.insert_one(mydict)
 cv2.destroyAllWindows()
 vs.stop()
