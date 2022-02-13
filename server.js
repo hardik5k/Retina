f5bad718f635899a3504023b3a7c83f1e74b626e
Hardik Khandelwal <77318140+hardik5k@users.noreply.github.com>
2022-02-13 13:51:07 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const cors = require('cors');
+const port = 5000;
+app.use(cors())
 
-app.get('/hii', (req, res) => {
-    res.send('Hello World!')
+app.get('/', (req, res) => {
+    res.json(x)
 })
 
 app.listen(port, () => {
